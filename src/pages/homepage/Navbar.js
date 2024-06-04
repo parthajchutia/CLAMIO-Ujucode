50c2c0cb403fdd7efe6843a369d7f6e150f41dc8
Partha_jyoti_chutia <119157042+parthajchutia@users.noreply.github.com>
2024-06-04 16:59:43 +0530

    Update Navbar.js

--- src/pages/homepage/Navbar.js
+++ src/pages/homepage/Navbar.js
@@ -18,8 +18,8 @@ const Navbar = () => {
 
       </div>
       <div className="flex items-center space-x-5 ml-auto font-bold">
-      <Link to="/explore" className="text-gray-700 hover:text-gray-900">Go to Explore</Link>
-<Link to="/refer" className="text-gray-700 hover:text-gray-900">Refer and Earn</Link>
+      <Link to="/" className="text-gray-700 hover:text-gray-900">Go to Explore</Link>
+<Link to="/Rewards" className="text-gray-700 hover:text-gray-900">Refer and Earn</Link>
         <IoIosNotifications className='size-8'/>
       </div>
     </nav>
